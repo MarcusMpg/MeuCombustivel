c185d76c0361c78852d2368cea9435a5b21d22f8
Marcus Mathues <107503566+marcusmpg@users.noreply.github.com>
2024-06-07 16:47:35 -0300

    Adicionar arquivos de projeto.

---
+++ MeuCombustivel/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿namespace MeuCombustivel
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_compensa_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                double etanol = Convert.ToDouble(txt_etanol.Text);
+                double gasolina = Convert.ToDouble(txt_gasolina.Text);
+                string msg = "";
+                if (etanol <= (gasolina * 0.7))
+                {
+                    msg = "O etanol esta compensndo mais";
+                }
+                else 
+                {
+                    msg = "A gasolina esta compensando mais";
+                }
+
+                DisplayAlert("Calculado", msg, "OK");
+
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("ops", ex.Message, "Fechar");
+            }
+        }
+    }
+
+}
